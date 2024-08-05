9e2b2474e5670fd2afe7ea0b641e7bc7cd6044b1
bes99 <pas1007@naver.com>
2024-08-05 12:19:12 +0900

    first commit

---
+++ src/main/java/sktflyai/translationserver/TranslationServerApplication.java
@@ -0,0 +1,13 @@
+package sktflyai.translationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sktflyai/translationserver/TranslationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sktflyai.translationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
